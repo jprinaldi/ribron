200db9bc47cdb30ffe0ee81dbf5e334daa2b01d3
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-07-26 13:31:42 -0300

    Update website

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.button-collapse').sideNav({closeOnClick: true});
+    $('.scrollspy').scrollSpy();
+    $('.modal-trigger').leanModal();
+    $('.parallax').parallax();
+});
